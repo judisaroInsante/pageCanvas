b890b72a0a830d54b6ba8bae5dbc861fb2fdd50f
Juan Dieo Sánchez <judisaro@hotmail.com>
2017-03-18 13:59:18 -0500

    Canvas page with no interaction in the client

---
+++ home.php
@@ -0,0 +1,105 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jegos en Canvas</title>
+    <link rel="stylesheet" href="css/foundation.css">
+    <link rel="stylesheet" href="css/app.css">
+
+   </head>
+
+  
+
+  <body>
+
+  <section id="contenedorTitulo">
+      <div class="callout text-center">
+        <img src="../assets/img/logos/logo.png">
+      </div>
+    </section>
+
+    <section id="contenedorMenu">
+        <ul class="menu dropdown fontTitle align-spaced"data-dropdown-menu>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Juegos en androd</a></li>
+            <li><a href="#">Retos</a></li>
+            <li><a href="#">crea y publica</a></li>
+            <li><a href="#">Registrarme/Login</a></li>
+        </ul>
+    </section>
+
+    
+  <section class="sliders">
+    <div class="expanded">
+      <div class="column">
+             <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+              <ul class="orbit-container">
+              <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
+              <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
+              <li class="is-active orbit-slide">
+                <img class="orbit-image" src="../assets/img/header/juego de la serpiente-header.PNG" alt="imagen slider">
+                <figcaption class="orbit-caption">Juegos canvas</figcaption>
+              </li>
+              <li class="orbit-slide">
+                <img class="orbit-image" src="../assets/img/header/juego ramdom-header.PNG" alt="imagen slider">
+                <figcaption class="orbit-caption">Juegos canvas</figcaption>
+              </li>
+              <li class="orbit-slide">
+                <img class="orbit-image" src="../assets/img/header/juego rardom2.png" alt="imagen slider">
+                <figcaption class="orbit-caption">Juegos canvas</figcaption>
+              </li>
+              <li class="orbit-slide">
+                <img class="orbit-image" src="../assets/img/header/juego pistolas.png" alt="imagen slider">
+                <figcaption class="orbit-caption">Apps android</figcaption>
+              </li>
+              </ul>
+              <nav class="orbit-bullets">
+              <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
+              <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
+              <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
+              <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
+            </nav>
+          </div>
+          </div>
+      </div>
+  </section>
+
+  <section>
+      <div style="background-color: #494B58; border-color: #494B58;" class="callout text-center">
+      </div>
+    </section>
+
+  <section>
+
+    <div class="row">
+       <div class="column small-12 columns">
+          <a href="juegos canvas/serpiente/serpiente.php">
+            <img class="box" src="../assets\img\img juegos\la serpiente.png">
+          </a>
+          
+        </div>
+    </div>  
+
+    <div class="row">
+       <div class="column small-12 columns">
+          <img class="box" src="../assets\img\img juegos\bomber man.png">
+        </div>
+    </div>  
+
+    <div class="row">
+       <div class="column small-12 columns">
+          <img class="box" src="../assets\img\img juegos\best city.png">
+        </div>
+    </div>   
+    
+  </section>  
+
+  
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/vendor/what-input.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script src="js/app.js"></script>
+  </body>
+</html>
